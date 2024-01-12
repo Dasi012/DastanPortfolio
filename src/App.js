1cc5d05b953de69d9f5e91f982aa6a522a534a16
Dasi012 <dastandulatbekuulu@gmail.com>
2024-01-12 17:52:15 +0600

    added component contact

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Banner from './components/Banner';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import About from './components/About';
-import Services from './components/Services';
-import Work from './components/Work';
-import Contact from './components/Contact';
+import React from "react";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Services from "./components/Services";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 const App = () => {
 	return (
-		<div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+		<div className="bg-site bg-no-repeat bg-cover overflow-hidden">
 			<Header />
 			<Banner />
 			<Nav />
@@ -17,7 +17,7 @@ const App = () => {
 			<Services />
 			<Work />
 			<Contact />
-			<div className='h-[4000px]'></div>
+			<div className="h-[4000px]"></div>
 		</div>
 	);
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Logo from '../assets/logo.svg';
+import React from "react";
+import Logo from "../assets/logo.svg";
 
 const Header = () => {
 	return (
-		<header className=' py-8 '>
-			<div className='container mx-auto'>
-				<div className='flex justify-between items-center'>
-					<a href='https:goggle.com'>
-						<img src={Logo} alt='Logo' />
+		<header className=" py-8 ">
+			<div className="container mx-auto">
+				<div className="flex justify-between items-center">
+					<a href="https:goggle.com">
+						<img src={Logo} alt="Logo" />
 					</a>
-					<button className='btn btn-sm'> Work with me </button>
+					<button className="btn btn-sm"> Work with me </button>
 				</div>
 			</div>
 		</header>
