39e6db5c10ab2f4814d2dec8ec5506bc82028d4a
Dasi012 <dastandulatbekuulu@gmail.com>
2024-01-13 14:36:04 +0600

    added new component contact

--- src/components/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { fadeIn } from "../variants";
-
-const Contact = () => {
-	return <section className="section" id="contact"></section>;
-};
-
-export default Contact;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { BiHomeAlt, BiUser } from 'react-icons/bi'
+import { BsClipboardData, BsBriefcase, BsChatSquareText } from 'react-icons/bs'
+import { Link } from 'react-scroll'
+const Nav = () => {
+	return (
+		<nav className='fixed bottom-2 lg-bottom-8 w-full z-50'>
+			<div className='container mt-auto'>
+				<div className='w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto flex px-5 justify-between text-2xl text-white/50 items-center'>
+					<Link
+						to='home'
+						activeClass='active'
+						smooth={true}
+						spy={true}
+						offset={-200}
+						className='cursor-pointer w-[60px] h-[60px] flex justify-center  items-center'
+					>
+						<BiHomeAlt />
+					</Link>
+					<Link
+						to='about'
+						activeClass='active'
+						smooth={true}
+						spy={true}
+						className='cursor-pointer w-[60px] h-[60px] flex justify-center items-center'
+					>
+						<BiUser />
+					</Link>
+					<Link
+						to='services'
+						activeClass='active'
+						smooth={true}
+						spy={true}
+						className='cursor-pointer w-[60px] h-[60px] flex justify-center items-center'
+					>
+						<BsClipboardData />
+					</Link>
+					<Link
+						to='work'
+						activeClass='active'
+						smooth={true}
+						spy={true}
+						className='cursor-pointer w-[60px] h-[60px] flex justify-center items-center'
+					>
+						<BsBriefcase />
+					</Link>
+					<Link
+						to='contact'
+						activeClass='active'
+						smooth={true}
+						spy={true}
+						className='cursor-pointer w-[60px] h-[60px] flex justify-center items-center'
+					>
+						<BsChatSquareText />
+					</Link>
+				</div>
+			</div>
+		</nav>
+	)
+}
+
+export default Nav
